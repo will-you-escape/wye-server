0fe52538df37eeb40f04eaabfea46c10f9ea1e2f
romgar <romain.garrigues.cs@gmail.com>
2017-09-05 20:43:03 +0100

    Commit initial structure for the wye server. Based on express-generator.

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Will you escape' });
+});
+
+module.exports = router;
