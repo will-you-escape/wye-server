f5343cc5ac837a550e4804bd7c18f624aeb32511
romgar <romain.garrigues.cs@gmail.com>
2017-09-06 09:00:31 +0100

    Authentication tests

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,25 @@
 var express = require('express');
+var connectEnsureLogin = require('connect-ensure-login');
 var router = express.Router();
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  connectEnsureLogin.ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
