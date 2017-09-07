423cb7a78aaf6ba217f47bdc514e0805458063dc
Paolo Escobar <paolo.escobar@hubesco.com>
2017-09-07 17:31:26 +0100

    Adds Heroku deployment

--- app.js
+++ app.js
@@ -43,4 +43,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
+});
+
 module.exports = app;
