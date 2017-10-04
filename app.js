2b77f2189dd69a06c818c8989b2ed300c772b6b4
romgar <romain.garrigues.cs@gmail.com>
2017-10-04 22:32:31 +0100

    Put back previous log errors

--- app.js
+++ app.js
@@ -56,15 +56,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  debug('there was an error');
-  res.status(500).send('Something broke!');
   // set locals, only providing error in development
-  //res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  //res.status(err.status || 500);
-  //res.render('error');
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 var mongoose = require('mongoose');
